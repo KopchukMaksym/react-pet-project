cb1411b11b7c486460d0a19a1fd828cae58acae0
Kopchuk Maksym <kopchuk.maksym@gmail.com>
2022-12-27 21:38:12 +0200

    add storybook to directory

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { Button } from "../stories/Button";
+
+export const App = () => {
+    return (
+        <div>
+            <>
+                <Button primary={false} label="fffffff" />
+                <h1>Hello Word ))) </h1>
+            </>
+        </div>
+    );
+};
